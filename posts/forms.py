1971303cc5fa6b0dbcc637f066cc074bb89ca0b3
Din <dinstamaly@gmail.com>
2021-09-30 18:35:25 +0600

    initial commit

---
+++ posts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .models import Post
+
+
+class PostModelForm(forms.ModelForm):
+    tags = forms.CharField(label='tag', required=False)
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+        ]
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) <= 3:
+            raise forms.ValidationError(
+                "Length has to be more than 3 character"
+            )
+        return title
